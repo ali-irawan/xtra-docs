dff5f621e4be322094b96a6f8465a4aadbb79153
Ali Irawan <boylevantz@gmail.com>
2019-01-02 23:31:30 +0700

    using dist as root

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(10000, function(){
+connect().use(serveStatic(__dirname + '/dist')).listen(10000, function(){
     console.log('Server running on 10000...');
 });
